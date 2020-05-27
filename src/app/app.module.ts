13c9cfe88d5088973380d730ee4202d056933bb0
Abhishek kandwal <Abhishek.kandwal@kelltontech.com>
2020-05-28 00:41:04 +0530

    app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material';
 
 import { AppComponent } from './app.component';
 import { ControllerComponent } from './controller/controller.component';
 import { SortingboardComponent } from './sortingboard/sortingboard.component';
 import { BarsComponent } from './bars/bars.component';
+import { HeaderComponent } from './common/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     ControllerComponent,
     SortingboardComponent,
-    BarsComponent
+    BarsComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
